c8c6255355c1b31d6b6b7086b87ffc1595c62736
Scott Feichter <scottfeichter@gmail.com>
2024-10-07 21:15:18 -0700

    misc (msg via sh alias)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Rust has keywords, similar to other languages...");
+
+    // appendix of rust keywords:
+    // https://doc.rust-lang.org/stable/book/appendix-01-keywords.html
+
+    let immutable = "immutable constant variable";
+
+    println!("{immutable}");
+
 }
